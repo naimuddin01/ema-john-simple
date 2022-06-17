dff1c98ea590b57f54c8e0d90c64ce452edff27b
naimuddin01 <103277035+naimuddin01@users.noreply.github.com>
2022-06-18 01:13:04 +0600

    finished

--- src/composents/Shop/Shop.js
+++ src/composents/Shop/Shop.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
 
 const Shop = () => {
+    
+    const first10 = fakeData.slice(0, 10); 
+
+    const [products, setProducts] = useState(first10)
+    console.log("Data", first10)
+
+    const [cart, setcart] = useState([])
+
+    const handleAddProduct = (products) =>{
+        console.log('product add', products)
+        const newCart = [...cart,products]
+        setcart(newCart);
+    } 
+
     return (
-        <div>
-            <h1>This is Shop</h1>
+        <div className='shop-container'>
+            <h3>{products.length}</h3>
+
+            <div className="product-container">
+                    {
+                        products.map(pd => <Product
+
+                             handleAddProduct = {handleAddProduct}
+                             product = {pd}>
+                             console.log("product",product);
+                             
+                            </Product>)  
+                    }
+                    
+            </div>
+
+            <div className="cart-container">
+                <Cart cart = {cart}></Cart>
+            </div>
+            
         </div>
     );
 };
