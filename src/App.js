3b73aeaf162cd95255eb4a30bb70da20fc7025a2
naimuddin01 <103277035+naimuddin01@users.noreply.github.com>
2022-06-20 21:48:35 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './composents/Header/Header';
 import Shop from './composents/Shop/Shop';
+import data from './data/data.json';
 
 function App() {
+
+  const [clubs, setClubs] = useState([]);
+
+  console.log('data', data);
+  useEffect(()=> {
+    setClubs(data);
+    console.log(data.first_name)
+  }, [])
+
   return (
     <div>
+
+      <h5>kire hosse na</h5>
+
+      <ul>
+        {
+          clubs.map(club => <li>{club.first_name}</li>)
+        }
+      </ul>
+
+
       <Header></Header>
       <Shop></Shop>
     </div>
